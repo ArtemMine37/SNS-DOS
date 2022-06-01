39a15ccfce60f772e4a32903aa9cf8eebc3bba0b
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2022-06-01 12:44:19 +0300

    Updated SNS-DOS boot file to 0.8.0 Build 1700

--- start_sns.bat
+++ start_sns.bat
@@ -1,7 +1,17 @@
 @echo off
+if "%1"=="/SnsUpdate" (
+if "%2"=="/NoUpdates" (
+goto boot
+)
+if "%2"=="/UpdateInstalled" (
+echo We're installed update. Enjoy the latest SNS-DOS!
+goto boot
+)
+)
+set UpdateLink=
 set UpdateID=080_b2_1
 set SnsUpdVer=0.8.0
-set SnsUpdateMode=1
+set SnsUpdateMode=0
 rem SnsUpdateMode is requied for checking updates.
 rem 0 - Disabled automatic update check.
 rem 1 - Automatic update check enabled,
@@ -10,7 +20,16 @@ rem choice to download updates.
 rem 2 - Automatic update install enabled.
 echo                     =SNS-DOS=
 echo Booting up...
+if "%SnsUpdateMode%"=="0" (
+goto boot
+) else (
 echo Checking for updates... If we found update, we message it.
+bitsadmin /create SnsUpdate
+bitsadmin /addfile SnsUpdate %UpdateLink% %cd%\update.cmd
+bitsadmin /RESUME SnsUpdate
+bitsadmin /complete SnsUpdate
+goto UpdChk
+)
 :boot
 if exist ru (
 goto sns-ru
@@ -44,3 +63,11 @@ sns_ru.cmd
 :sns-en
 cd sns_eng
 sns_en.cmd
+
+:UpdChk
+if "%SnsUpdateMode%"=="1" (
+update.cmd
+)
+if "%SnsUpdateMode%"=="2" (
+update.cmd /AutoDL
+)
