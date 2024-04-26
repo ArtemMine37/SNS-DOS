cc9d4a242ad9541d3e084291388d3d9d8bb36e90
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2024-04-27 00:38:23 +0300

    Rewrite

--- snsui/browse.cmd
+++
@@ -1,4 +0,0 @@
-echo [SnsBrowser / Network status: ]
-if not exist "%temp%\SnsUI\BrowserData" (md "%temp%\SnsUI" & md "%temp%\SnsUI\BrowserData")
-curl --output "%temp%\SnsUI\BrowserData\domainlist.cmd"
-curl --output "%temp%\SnsUI\BrowserData\Offline.7z"
--- snsui/usrcfg.bat
+++
@@ -1,9 +0,0 @@
-:: IN ALPHA TEST
-:: Currently not used, so you can safely remove this file.
-:: SNS-DOS User Config
-set EnableNoUserMode=1
-
-
-
-if "%UnlockSettings%"=="1" (
-) else (goto InBeta)
